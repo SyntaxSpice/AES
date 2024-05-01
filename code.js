fbe70f96b50f8fd9e30219077b493e70145378f5
ESCowboy <ortega31294@gmail.com>
2024-05-01 23:45:11 +0200

    enchant

--- code.js
+++ code.js
@@ -1,1 +1,41 @@
-alert('hello');
+let total = 0;
+let current = 0;
+console.log(1);
+const bar = document.getElementById('active-bar');
+const cVal = document.getElementById('c-value');
+const tVal = document.getElementById('t-value');
+const startBtn = document.getElementById('start');
+
+const animate = () => {
+  return new Promise((resolve) => {
+    let state = 0;
+    const render = (e) => {
+      if(state >= 100 ) {
+        bar.style.width = `${100}%`;
+        resolve(true)
+        return;
+      }
+  
+      state += 1;
+      bar.style.width = `${state}%`;
+      requestAnimationFrame(render)
+    }
+    render();
+  })
+}
+
+animate();
+
+const enchant = async () => {
+  await animate();
+  total++;
+  if(current < 3) {
+    current++
+  } else {
+    current = Math.random() > 0.5 ? current + 1 : 0;
+  }
+  cVal.innerHTML = current;
+  tVal.innerHTML = total;
+}
+
+startBtn.addEventListener('click', enchant)
