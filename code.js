dda921716f5e19d89aad14f264583803d07478ed
ESCowboy <ortega31294@gmail.com>
2024-05-02 00:13:18 +0200

    improvements

--- code.js
+++ code.js
@@ -1,18 +1,20 @@
 let total = 0;
 let current = 0;
+let best = 0;
+
 let disabled = false;
-console.log(1);
 const bar = document.getElementById('active-bar');
 const cVal = document.getElementById('c-value');
 const tVal = document.getElementById('t-value');
+const bVal = document.getElementById('b-value');
 const startBtn = document.getElementById('start');
 
 const animate = () => {
   return new Promise((resolve) => {
     let state = 0;
     const render = (e) => {
-      if(state >= 100 ) {
-        bar.style.width = `${100}%`;
+      if(state >= 99 ) {
+        bar.style.width = `${99.6}%`;
         resolve(true)
         return;
       }
@@ -35,8 +37,14 @@ const enchant = async () => {
   } else {
     current = Math.random() > 0.5 ? current + 1 : 0;
   }
+
+  if(current > best) {
+    best = current;
+  }
+
   cVal.innerHTML = current;
   tVal.innerHTML = total;
+  bVal.innerHTML = best;
   disabled = false;
 }
 
