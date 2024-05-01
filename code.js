f20b24375da6ec0d981ebe7c0e409c08ddb4203e
ESCowboy <ortega31294@gmail.com>
2024-05-02 00:47:12 +0200

    update animation

--- code.js
+++ code.js
@@ -8,17 +8,18 @@ const cVal = document.getElementById('c-value');
 const tVal = document.getElementById('t-value');
 const bVal = document.getElementById('b-value');
 const startBtn = document.getElementById('start');
+const vipBtn = document.getElementById('vip');
 
 const animate = () => {
   return new Promise((resolve) => {
     let state = 0;
     const render = (e) => {
-      if(state >= 99 ) {
+      if (state >= 99) {
         bar.style.width = `${99.6}%`;
         resolve(true)
         return;
       }
-  
+
       state += 3;
       bar.style.width = `${state}%`;
       requestAnimationFrame(render)
@@ -27,20 +28,28 @@ const animate = () => {
   })
 }
 
-const enchant = async () => {
-  if(disabled) return;
+const scaleAnim = (el) => {
+  el.classList.add('pulse');
+  setTimeout(() => el.classList.remove('pulse'), 1000)
+}
+
+const enchant = async (chance) => {
+  if (disabled) return;
   disabled = true;
   await animate();
   total++;
-  if(current < 3) {
+  if (current < 3) {
     current++
   } else {
-    current = Math.random() > 0.5 ? current + 1 : 0;
+    current = Math.random() > (1 - chance / 100) ? current + 1 : 0;
   }
 
-  if(current > best) {
+  if (current > best) {
     best = current;
+    scaleAnim(bVal);
   }
+
+  scaleAnim(cVal);
 
   cVal.innerHTML = current;
   tVal.innerHTML = total;
@@ -48,4 +57,8 @@ const enchant = async () => {
   disabled = false;
 }
 
-startBtn.addEventListener('click', enchant)
+
+
+startBtn.addEventListener('click', () => enchant(50))
+vipBtn.addEventListener('click', () => enchant(75))
+
